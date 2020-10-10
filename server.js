e968d3d99d52b7af31bf2428b570ca438001807c
Dmitry <korndima@tut.by>
2020-10-10 21:52:44 +0300

    feat: add POST/login route

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
 ]
+
+app.post('/login', (req, res)=>{
+	const mockUsername = 'billyTheKid';
+	const mockPassword = 'superSecret';
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (username === mockUsername && password === mockPassword){
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encripted token goes here'
+		});
+	} else {
+		res.json({
+			success: false,
+			message: 'Warn: password or username does not match!'
+		})
+	}
+
+})
+
+
 
 app.get('/users',(req,res)=>{
 	res.json({
